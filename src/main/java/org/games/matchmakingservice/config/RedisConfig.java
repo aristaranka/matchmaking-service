803348fef67839284568310ff88a25c76ca36f22
aristaranka05 <aristaranka@pyramyd.ai>
2025-08-03 00:54:19 -0500

    java version 17, enhancements in config file and dependencies changes

--- src/main/java/org/games/matchmakingservice/config/RedisConfig.java
+++ src/main/java/org/games/matchmakingservice/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package org.games.matchmakingservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,9 +12,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String redisHost;
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
@@ -28,6 +34,8 @@ public class RedisConfig {
         // Use JSON serializer for values
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        template.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
         
         template.afterPropertiesSet();
         return template;
